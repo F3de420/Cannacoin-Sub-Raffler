8144721e21e65a24208bdcd4543729f390c4170a
F3de420 <136014568+F3de420@users.noreply.github.com>
2024-11-02 13:28:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,22 @@ import os
 import praw
 
 # Configuration using environment variables
-USERAGENT = 'Bot for managing raffle on a subreddit'
+USERAGENT = 'Bot for raffle management on subreddit'
 APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv("APP_SECRET")
 APP_REFRESH = os.getenv("APP_REFRESH")
+REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
+REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
 
 def login():
-    """Log in to Reddit using refresh token authentication."""
+    """Logs into Reddit using refresh token authentication."""
     return praw.Reddit(
         user_agent=USERAGENT,
         client_id=APP_ID,
         client_secret=APP_SECRET,
         refresh_token=APP_REFRESH,
+        username=REDDIT_USERNAME,
+        password=REDDIT_PASSWORD
     )
 
 def is_moderator(reddit_instance, username, subreddit_name):
