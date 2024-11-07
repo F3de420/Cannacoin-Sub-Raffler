acf5e9418528c1659b687c6749a35ceaca2152fe
F3de420 <136014568+F3de420@users.noreply.github.com>
2024-11-07 15:10:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import praw
 
-USERAGENT = 'Bot for raffle management on subreddit'
+USERAGENT = 'Bot for managing raffle on subreddits'
 APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv("APP_SECRET")
 APP_REFRESH = os.getenv("APP_REFRESH")
@@ -9,7 +9,7 @@ REDDIT_USERNAME = os.getenv("REDDIT_USERNAME")
 REDDIT_PASSWORD = os.getenv("REDDIT_PASSWORD")
 
 def login():
-    """Logs into Reddit using environment variables."""
+    """Log in to Reddit using environment variables."""
     return praw.Reddit(
         user_agent=USERAGENT,
         client_id=APP_ID,
@@ -20,7 +20,7 @@ def login():
     )
 
 def is_moderator(reddit_instance, username, subreddit_name):
-    """Checks if the user is a moderator of the specified subreddit."""
+    """Checks whether the user is a moderator of the specified subreddit."""
     subreddit = reddit_instance.subreddit(subreddit_name)
     moderators = subreddit.moderator()
     return username in [mod.name for mod in moderators]
