5159fe9e39722995e468633f2d56cea46b3f2a9d
F3de420 <136014568+F3de420@users.noreply.github.com>
2024-11-15 18:32:35 +0100

    Update Cannacoin-Sub-Raffler.py

--- Cannacoin-Sub-Raffler.py
+++ Cannacoin-Sub-Raffler.py
@@ -1,4 +1,4 @@
-# bot.py
+# Cannacoin-Sub-Raffler.py
 
 import threading
 import time
@@ -6,33 +6,33 @@ import sys
 import logging
 from logging.handlers import RotatingFileHandler
 
-from config.config import load_config, save_config, CONFIG_FILE
+from config.config import load_config, save_config
 from utils.authentication import connect_to_reddit
 from utils.spinner import spinner_animation
 from handlers.subreddit_monitor import monitor_subreddit
 
-# Logging configuration
+# Configurazione del logging
 log_handler = RotatingFileHandler("logs/bot.log", maxBytes=5 * 1024 * 1024, backupCount=5)
 log_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(log_handler)
 
-logger.info("Logging initialized.")
+logger.info("Logging inizializzato.")
 
-# Load configuration data
+# Carica i dati di configurazione
 data = load_config()
 
-# Global configurations
+# Configurazioni globali
 SUBREDDITS = data["config"]["subreddits"]
 
-# Start the bot
+# Avvia il bot
 if __name__ == "__main__":
-    logger.info("Starting bot.")
+    logger.info("Avvio del bot.")
     try:
-        # Create an event to signal the spinner to stop
+        # Crea un evento per segnalare allo spinner di fermarsi
         spinner_stop_event = threading.Event()
-        # Start the spinner in a separate thread
+        # Avvia lo spinner in un thread separato
         spinner_thread = threading.Thread(target=spinner_animation, args=(spinner_stop_event,))
         spinner_thread.start()
 
@@ -44,11 +44,11 @@ if __name__ == "__main__":
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        logger.info("Bot stopped by user.")
+        logger.info("Bot interrotto dall'utente.")
         spinner_stop_event.set()
         spinner_thread.join()
     except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
+        logger.error(f"Si è verificato un errore imprevisto: {e}")
         spinner_stop_event.set()
         spinner_thread.join()
         sys.exit(1)
