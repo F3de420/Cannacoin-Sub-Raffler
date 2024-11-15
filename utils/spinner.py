c0531a7e3342b83630836d5749e5ed1f8f5ff5ca
F3de420 <136014568+F3de420@users.noreply.github.com>
2024-11-15 18:16:16 +0100

    Create spinner.py

---
+++ utils/spinner.py
@@ -0,0 +1,15 @@
+# utils/spinner.py
+
+import sys
+import time
+import itertools
+
+def spinner_animation(stop_event):
+    """Displays a spinner animation in the console."""
+    spinner = itertools.cycle(['|', '/', '-', '\\'])
+    while not stop_event.is_set():
+        sys.stdout.write('\r' + next(spinner) + ' Bot is running...')
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rBot stopped.            \n')
+    sys.stdout.flush()
